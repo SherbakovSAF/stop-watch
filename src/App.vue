b2a5279b197aeff75215669f2fdc20b41fe741d2
SherbakovSAF <sherbakovsaf@gmail.com>
2023-03-26 00:37:13 +0300

    Первостепенная вёрстка

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="container">
     <div class="card__wrap">
-      <article>
+      <article v-for="stopWatch in stopWatchesState" :key="stopWatch.id">
         <div class="stopWatchCard">
           <div class="stopWatchCard__contentWrap">
-            <h1>2:40</h1>
+            <h1>{{stopWatch.count}}</h1>
             <hr>
             <div class="control__panel">
-              <resetButton svgFill="#FF23"/>
+              <startButton v-if="!stopWatch.isLaunch" svgFill='#fff'/>
+              <pauseButton v-if="stopWatch.isLaunch" svgFill='#fff'/>  
+              <resetButton  svgFill='#fff'/>
             </div>
           </div>
         </div>
@@ -18,26 +20,87 @@
 
 <script>
 import resetButton from './components/resetButton.vue';
+import pauseButton from './components/pauseButton.vue';
+import startButton from './components/startButton.vue';
 
 export default {
   name: 'App',
   components: {
-    resetButton
+    resetButton,
+    pauseButton,
+    startButton
+  },
+  data() {
+    return {
+      stopWatchesState: [{
+        id: 0,
+        count: 2323,
+        isLaunch: false
+      }, {
+        id: 1,
+        count: 5435,
+        isLaunch: false
+      }, {
+        id: 2,
+        count: 235435345323,
+        isLaunch: false
+      },]
+    }
   }
 }
 </script>
 
 <style>
+:root {
+  --colorActive: #FFF;
+  --colorInActive: #9E9E9E;
+}
+ 
 html {
   background-color: #353638;
+
 }
+
+html * {
+  margin: 0;
+  padding: 0;
+}
+
 .container {
   max-width: 775px;
   margin: 0 auto;
+  padding-top: 50px;
 }
 
 .card__wrap {
   display: flex;
   flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.card__wrap article{
+width: 225px;
+background-color: #696969;
+}
+
+.card__wrap article h1 {
+
+  
+  font-family: 'Gotham Pro';
+  text-align: center;
+  color: var(--colorInActive);
+  padding: 22px 0px 20px 0px;
+  font-size: 22px;
+}
+
+
+.control__panel{
+  display: flex;
+  justify-content: center;
+  padding: 20px 0px;
+}
+
+.control__panel svg:last-child{
+  margin-left: 48px;
 }
 </style>
--- src/components/resetButton.vue
+++ src/components/resetButton.vue
@@ -1,17 +1,17 @@
 <template>
-<svg width="17" height="20" viewBox="0 0 17 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-     <path d="M0 20V0L17 10L0 20Z" :fill="svgFill"/>
-</svg>     
+     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="20" height="20" :fill="svgFill" />
+     </svg>
 </template>
 
 <script>
-export default{
+export default {
      name: "ResetButton",
      props: {
           svgFill: {
                type: String,
                required: false,
-               default: function(){
+               default: function () {
                     return "9E9E9E"
                }
           }
