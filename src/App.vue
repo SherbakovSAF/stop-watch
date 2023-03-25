d163e09cb88ea50da23066995d43a40d641cf5a4
SherbakovSAF <sherbakovsaf@gmail.com>
2023-03-25 23:53:43 +0300

    Инициализация репозицитория

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div class="card__wrap">
+      <article>
+        <div class="stopWatchCard">
+          <div class="stopWatchCard__contentWrap">
+            <h1>2:40</h1>
+            <hr>
+            <div class="control__panel">
+              <resetButton svgFill="#FF23"/>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+import resetButton from './components/resetButton.vue';
+
+export default {
+  name: 'App',
+  components: {
+    resetButton
+  }
+}
+</script>
+
+<style>
+html {
+  background-color: #353638;
+}
+.container {
+  max-width: 775px;
+  margin: 0 auto;
+}
+
+.card__wrap {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
